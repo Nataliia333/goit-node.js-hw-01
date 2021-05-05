8b9adf139b72fbb0089deb35e8215d292c7172ea
Nataliia333 <n.o.prykhodko@gmail.com>
2021-05-05 23:29:06 +0300

    функции для работы с контактами

--- contacts.js
+++ contacts.js
@@ -1,21 +1,67 @@
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
+import fs from "fs";
+import path from "path";
+
+const contactsPath = path.join("./db", "contacts.json");
 
 // TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find(({ id }) => String(id) === contactId);
+    console.table(contact);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const newListContacts = JSON.stringify(
+      contacts.filter(({ id }) => String(id) !== contactId),
+      null,
+      2
+    );
+    await fs.writeFile(contactsPath, newListContacts);
+
+    const contactUpdated = await fs.readFile(contactsPath);
+    console.table(JSON.parse(contactUpdated));
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const newContact = { id: shortid.generate(), name, email, phone };
+    const newContactsList = JSON.stringify([...contacts, newContact], null, 2);
+
+    await fs.writeFile(contactsPath, newContactsList);
+
+    const dataUpdated = await fs.readFile(contactsPath);
+    console.table(JSON.parse(dataUpdated));
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-import program from "./utils/commander.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+// import program from "./utils/commander.js";
+import commander from "commander";
+const { program } = commander;
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 program.parse(process.argv);
 
 const argv = program.opts();
@@ -7,19 +22,19 @@ const argv = program.opts();
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      listContacts();
       break;
 
     case "get":
-      // ... id
+      getContactById(id);
       break;
 
     case "add":
-      // ... name email phone
+      addContact(name, email, phone);
       break;
 
     case "remove":
-      // ... id
+      removeContact(id);
       break;
 
     default:
