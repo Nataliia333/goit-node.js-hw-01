3db39dc314f540a6df2075de1d48a5d7cc3c9921
Nataliia333 <n.o.prykhodko@gmail.com>
2021-05-05 16:41:26 +0300

    start work

---
+++ index.js
@@ -0,0 +1,30 @@
+import program from "./utils/commander.js";
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
